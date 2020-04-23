b4505fe6abccce148aeaec009364c2ae6aa8394d
thomasdroege <ThomasDroege@gmx.de>
2020-04-23 22:19:52 +0200

    Added mapbox geocode request

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=2688fe4816b5c5768f1a5b371017bab2&query=Dortmund&units=m'
+const weatherURL = 'http://api.weatherstack.com/current?access_key=2688fe4816b5c5768f1a5b371017bab2&query=Dortmund&units=m'
 
-request({url: url, json: true}, (error, response) => {
+request({url: weatherURL, json: true}, (error, response) => {
     console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature, "degrees out. It feels like "+ response.body.current.feelslike+ " degree out.")
 })
+
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZHJ0aG9tYXNkciIsImEiOiJjazlkNjJ2cHUwMGJtM25zMGVsMmtpcTg0In0.wloD6fSV5yJVRSQO54w1Jg&limit=1'
+
+request({url: geocodeURL, json: true}, (error, response ) => {
+    const latitude = response.body.features[0].center[1]
+    const longitude =  response.body.features[0].center[0]
+    console.log(latitude, longitude)
+})
+
+

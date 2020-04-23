06d5fb40613143d58a57b4ba41f0f86db60958ff
thomasdroege <ThomasDroege@gmx.de>
2020-04-23 21:33:38 +0200

    extended the http request.

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=2688fe4816b5c5768f1a5b371017bab2&query=New%20York'
+const url = 'http://api.weatherstack.com/current?access_key=2688fe4816b5c5768f1a5b371017bab2&query=Dortmund&units=m'
 
-request({url: url}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({url: url, json: true}, (error, response) => {
+    console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature, "degrees out. It feels like "+ response.body.current.feelslike+ " degree out.")
 })

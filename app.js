e4fdfbd467e552edfb257f63822701b52c32e4af
thomasdroege <ThomasDroege@gmx.de>
2020-04-28 22:34:14 +0200

    Refactoring by destructuring objects.

--- app.js
+++ app.js
@@ -2,20 +2,20 @@ const request = require('request')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-const location = process.argv[2]
+const address = process.argv[2]
 
-if(!location){
+if(!address){
     console.log('Please provide a location.')
 } else{
-    geocode(location, (error, geoData) =>{
+    geocode(address, (error, {latitude, longitude, location} = {}) =>{
         if(error){
             return console.log(error)
         }     
-        forecast(geoData.latitude, geoData.longitude, (error, forecastData) => {
+        forecast(latitude, longitude, (error, forecastData) => {
             if(error){
                 return console.log(error)
             }
-            console.log(geoData.location)
+            console.log(location)
             console.log(forecastData)
             })
     })

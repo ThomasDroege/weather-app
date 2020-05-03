0737c15ee7f415cccf454afc6a7eb197319e4ced
thomasdroege <ThomasDroege@gmx.de>
2020-05-03 22:37:39 +0200

    added 404 view

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -51,8 +51,23 @@ app.get('/weather', (req, res) => {
     })
 })
 
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        errorMessage: 'Help article not found',
+        name: 'Thomas D.'
+    })
+})
 
-
+// 404 Handler
+// wildcard: * = match everything that didnt get matched before
+app.get('*', (req, res) =>{
+    res.render('404', {
+        title: '404',
+        errorMessage: 'Page not found',
+        name: 'Thomas D.'
+    })
+})
 
 
 app.listen(3000, () =>{

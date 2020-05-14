12cb465cb05d3d10af2a872f1a4d26947cb68278
thomasdroege <ThomasDroege@gmx.de>
2020-05-14 22:10:26 +0200

    Fetched weater data from static http request

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,22 @@
 console.log('Client side javascript file is loaded.')
+
+fetch('http://puzzle.mead.io/puzzle').then((response) => {
+    response.json().then( (data) => {
+        console.log(data)
+    })
+})
+
+fetch('http://localhost:3000/weather?address=boston').then( (response) =>{
+    response.json().then( (data) =>{
+        if(data.error){
+            console.log('error')    
+        }
+        else{
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+        
+    })
+})
+
+{error: "Unable to find location. Try another search."}

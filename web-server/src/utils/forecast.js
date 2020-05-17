66946b3614d829962f31573f96ef99542131028f
thomasdroege <ThomasDroege@gmx.de>
2020-05-17 21:16:54 +0200

    added humidity to weather forecast

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         }else if(body.error){
             callback('Unable to find location. Try another search.', undefined)
         } else{
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like "+ body.current.feelslike+ " degree out.")
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + "°C out. It feels like "+ body.current.feelslike + " degree out. The humidity is " + body.current.humidity + "%.")
         }
     })
 }
